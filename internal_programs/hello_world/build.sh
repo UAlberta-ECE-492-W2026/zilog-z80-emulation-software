d77cb42b7ab88db6df168442fe99df15170452ca
Nikolai <nikolai@insidedesk.com>
2026-02-23 09:05:36 -0700

    add makefile and script for converting to vivado format, but add files this time

--- internal_programs/hello_world/build.sh
+++ internal_programs/hello_world/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # minimal compiler args
 zcc +z80 -clib=classic hello_world.c -mz80_strict -v -m -create-app 
+
+# stick compiled binary in a text file
+z88dk-dis -o 0x0100 -x a.map a.rom > a.txt
