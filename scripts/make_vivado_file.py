5e70ab2b4f1fda7e8f8d5756024fa4bfb2575be0
Nikolai <nikolai@insidedesk.com>
2026-04-02 21:04:17 -0600

    update the binary file generator for verilator compatibility

--- scripts/make_vivado_file.py
+++ scripts/make_vivado_file.py
@@ -1,9 +1,10 @@
 import os
 import sys
 
-def main(input_file, output_file):
+def main(input_file, output_file, target_length):
     f_in = open(input_file, 'r')
-    f_out = open(output_file, 'w')
+    
+    bytes_to_write = []
     for l in f_in:
         l = l.strip().split(';')
         if len(l) <= 1:
@@ -15,14 +16,25 @@ def main(input_file, output_file):
         l = l[1:]
         
         for b in l:
-            f_out.write(b + '\n')
+            bytes_to_write +=['{0:08b}'.format(int.from_bytes((bytes.fromhex(b))))]
     
     f_in.close()
+
+    if len(bytes_to_write) > target_length:
+        print("Binary is too big!")
+        return
+    
+    #bytes_to_write += ['00'] * (target_length - len(bytes_to_write))
+    print("Write %d bytes as strings to %s" % (len(bytes_to_write), output_file))
+
+    f_out = open(output_file, 'w')
+    f_out.write('\n'.join(bytes_to_write))
     f_out.close()
 
 if __name__ == "__main__":
     print('start')
     # should be a path to a .txt as input and a .vivado (really also just text) as output
     # the input should be produced by z88dk-dis
-    main(sys.argv[1], sys.argv[2])
+    # the 'magic number' is the number of bytes that fit in the program ram.
+    main(sys.argv[1], sys.argv[2], 60415 - 256)
     print("done")
